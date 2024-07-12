21138b44298f6f8c4f5e9ad636ebbd374c707963
yasiruaf <muhammadyasir4741@gmail.com>
2024-07-12 22:42:28 +0500

    yasir title change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Yasir';
+export class AppComponent implements OnInit {
+  // title = 'Yasir';
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.setTitle('Yasir');
+  }
+  setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
